4231f77758bf1ae7b730d8f4d809ffeb54c8a26c
PC-17 <ahijazi083@gmail.com>
2025-10-02 10:29:02 +0300

    Barbies project

---
+++ app/src/main/java/com/example/myapptamar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapptamar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private Button btn1,btn2,btn3,btn4;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=findViewById(R.id.imageView);
+        btn1=findViewById(R.id.button);
+        btn2=findViewById(R.id.button7);
+        btn3=findViewById(R.id.button6);
+        btn4=findViewById(R.id.button5);
+        btn1..
+    }
+}
